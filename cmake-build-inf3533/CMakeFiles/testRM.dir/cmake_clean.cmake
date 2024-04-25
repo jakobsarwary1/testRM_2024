62e85515042793ab865a72f9aa48832dc5cc304b
jakobsarwary1 <jakobsarary@gmail.com>
2024-04-25 20:14:10 +0200

    test

---
+++ cmake-build-inf3533/CMakeFiles/testRM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testRM.dir/main.c.o"
+  "CMakeFiles/testRM.dir/main.c.o.d"
+  "testRM"
+  "testRM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testRM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
